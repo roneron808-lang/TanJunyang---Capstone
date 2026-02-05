8205d4e7830dfdb2d2e4ca6fc855657afb870ac4
roneron808-lang <roneron808@gmail.com>
2026-02-05 22:03:15 +0800

    capstoneproject(part2)

---
+++ finance-project2/my-react-app/src/App.jsx
@@ -0,0 +1,21 @@
+import { StockProvider } from './context/StockContext';
+import StockForm from './components/StockForm';
+import StockList from './components/StockList';
+import './App.css';
+
+function App() {
+  return (
+    <StockProvider>
+      <div className="app">
+        <header className="app-header">
+          <img src="/profile_pic.jpg" width="800" height="250" alt="Profile" />
+          <h1>Finance Dashboard</h1>
+        </header>
+        <StockForm />
+        <StockList />
+      </div>
+    </StockProvider>
+  );
+}
+
+export default App;
